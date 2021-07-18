abb8dbbabb2dc27f12535dba57cb2595aad19659
Artem <uralskiyart@gmail.com>
2021-07-18 19:31:04 +0300

    hw1 is done!

--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
-from .models import User
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from .serializers import UserSerializer
 
 
+from .models import User
+
+
 class UserViewSet(ModelViewSet):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     serializer_class = UserSerializer
     queryset = User.objects.all()
