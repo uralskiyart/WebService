5c39cf6230f7eb73967035ebf7a11436babc2e6e
Artem <uralskiyart@gmail.com>
2021-07-26 03:13:41 +0300

    Create models for todo app

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(verbose_name='Название проекта', max_length=256)
+    users_on_projects = models.ManyToManyField(User, verbose_name='Пользователей учавствуют')
+    repository_link = models.URLField(verbose_name='Адрес репозитория')
+    description = models.TextField(verbose_name='Описание продукта', max_length=64, blank=True, null=True)
+
+
+class Todo(models.Model):
+    project_name = models.OneToOneField(Project, verbose_name='Проект заметки', on_delete=models.CASCADE)
+    text = models.TextField(verbose_name='Содержимое заметки', max_length=1024, blank=True, null=True)
+    author = models.OneToOneField(User, verbose_name='Автор заметки', on_delete=models.DO_NOTHING)
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateField(auto_now_add=True, verbose_name='Дата последнего обновления')
+    is_active = models.BooleanField(default=True, verbose_name='Состояние заметки')
--- users/models.py
+++ users/models.py
@@ -4,3 +4,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.CharField(verbose_name='эл. почта пользователя', max_length=256, unique=True)
+
