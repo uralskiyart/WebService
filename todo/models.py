b8b971e638066911e70b204cea2413d87e97b518
Artem <uralskiyart@gmail.com>
2021-07-26 17:23:54 +0300

    rewrite model Todo

--- todo/models.py
+++ todo/models.py
@@ -6,13 +6,13 @@ class Project(models.Model):
     name = models.CharField(verbose_name='Название проекта', max_length=256)
     users_on_projects = models.ManyToManyField(User, verbose_name='Пользователей учавствуют')
     repository_link = models.URLField(verbose_name='Адрес репозитория')
-    description = models.TextField(verbose_name='Описание продукта', max_length=64, blank=True, null=True)
+    description = models.TextField(verbose_name='Описание проекта', max_length=64, blank=True, null=True)
 
 
 class Todo(models.Model):
-    project_name = models.OneToOneField(Project, verbose_name='Проект заметки', on_delete=models.CASCADE)
+    project_name = models.ManyToManyField(Project, verbose_name='Проект заметки')
     text = models.TextField(verbose_name='Содержимое заметки', max_length=1024, blank=True, null=True)
-    author = models.OneToOneField(User, verbose_name='Автор заметки', on_delete=models.DO_NOTHING)
+    authors = models.ManyToManyField(User, verbose_name='Автор(ы) заметки')
     created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateField(auto_now_add=True, verbose_name='Дата последнего обновления')
     is_active = models.BooleanField(default=True, verbose_name='Состояние заметки')
